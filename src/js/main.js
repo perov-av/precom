bffc9747ac989b250dc83c95bcc6e1ba843b7e62
Alexandr Perov <PerovAV.home@gmail.com>
2024-02-25 00:33:16 +0300

    Сделана шапка, баннер и футер

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,45 @@
-const sum = (a, b) => a + b;
-console.log(sum(2, 10));
+function toggleItem(elem) {
+  for (var i = 0; i < elem.length; i++) {
+    elem[i].addEventListener("click", function(e) {
+      var current = this;
+      for (var i = 0; i < elem.length; i++) {
+        if (current != elem[i]) {
+          elem[i].classList.remove('active');
+        } else if (current.classList.contains('active') === true) {
+          current.classList.remove('active');
+        } else {
+          current.classList.add('active')
+        }
+      }
+      e.preventDefault();
+    });
+  };
+}
+toggleItem(document.querySelectorAll('.menu__list-item'));
+
+
+$(document).ready(function(){
+
+// Удаляем класс active у всех пунктов меню при клике на логотип
+	$('.logo').click(function () {
+		$('.menu__list-item').removeClass('active')
+	});
+
+// Подключаем слайдер slick для баннера
+$('.banner-slider').slick({
+	dots: true,
+  infinite: true,
+  speed: 1300,
+  fade: false,
+  cssEase: 'linear',
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 3000
+});
+
+
+
+});
+
+
+
