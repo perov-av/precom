58e754911cea4869100a714694d4fd26f001aa22
Alexandr Perov <PerovAV.home@gmail.com>
2024-03-19 21:17:01 +0300

    Сделана страница одиночного товара

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,9 @@ toggleItem(document.querySelectorAll('.equipment__tab'));
 // Пункты меню
 toggleItem(document.querySelectorAll('.menu__list-item'));
 
+// Кнопки фильтра
+toggleItem(document.querySelectorAll('.category__filter-item'));
+
 
 
 $(document).ready(function(){
@@ -33,16 +36,42 @@ $(document).ready(function(){
 	});
 
 // Подключаем слайдер slick для баннера
-$('.banner-slider').slick({
-	dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear',
-	arrows: false,
-	autoplay: true,
-	autoplaySpeed: 3000
-});
+  $('.banner-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+
+// Магнифик попаб на галерею фоток товара
+  $('.sigle-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: false,
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">Фотография #%curr%</a> не может быть загружена.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + ' ' + '<small>ООО "Преком"</small>';
+      }
+    }
+  });
+
+  // Всплывающая форма
+  $('.popupForm').magnificPopup({
+		type:'inline',
+		midClick: true,
+		// mainClass: 'custom-popup-class'
+	});
 
 
 
